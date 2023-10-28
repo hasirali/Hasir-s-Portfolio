5db7382364c660f541349ee802842c5e5778de9e
hasirali <hasira804@gmail.com>
2023-10-28 18:06:37 +0530

    dekstopDone

--- HasirPortfolio/src/Pages/Home.jsx
+++ HasirPortfolio/src/Pages/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
         Hello and welcome to my Portfolio!! I am Hasir Ali, a student pursuing an undergraduate degree in Information Technology at the United College of Engineering and Research. I have skills in video editing using Adobe Premiere Pro, programming in C/C++, web development and proficiency in Git and GitHub for version control. I also have a strong grasp of Data Structures and Algorithms, and I'm a content creator with over 30,000 followers on Instagram. Join me on my journey through the world of technology and content creation. 
         </p>
         <div class="header__btn  ">
-          <button class="btn"><a href="https://drive.google.com/file/d/1lFwzCKVcGcM2BbRFmkxQ9DPKpYu50F3P/view?usp=drive_link" class="btn">My Resume</a></button>
+          <button class="btn"><a href="https://drive.google.com/file/d/1lFwzCKVcGcM2BbRFmkxQ9DPKpYu50F3P/view?usp=drive_link" class="btn" target='_blank'>My Resume</a></button>
         </div>
       </div>
     </div>
--- HasirPortfolio/vite.config.js
+++ HasirPortfolio/vite.config.js
@@ -1,8 +1,7 @@
-git import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
 })
